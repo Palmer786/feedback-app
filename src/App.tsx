edaecab5ad6d5adcae05ab5ea4c17c2d808aecfb
Palmer786 <palmer786786@gmail.com>
2021-03-04 15:13:01 +0100

    feat: Add left section with logo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,30 @@
 import React from "react";
+import { createGlobalStyle, ThemeProvider } from "styled-components";
+import LeftSection from "./components/LeftSection";
+
+const GlobalStyle = createGlobalStyle`
+    * {
+      // 1 rem = 10px
+      font-size: 62.5%;
+      box-sizing: border-box;
+    }
+    body {
+      margin: 0;
+      padding: 0;
+    }
+    html {
+      scroll-behavior: smooth;
+    }
+`;
+
+const theme = {};
 
 const App: React.FC = () => {
   return (
-    <div>
-      <h1>App</h1>
-    </div>
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <LeftSection />
+    </ThemeProvider>
   );
 };
 
---
+++ src/components/LeftSection/index.tsx
@@ -0,0 +1,30 @@
+import React from "react";
+import styled from "styled-components";
+import logo from "../../images/logo.png";
+
+const Wrapper = styled.div`
+  width: 25%;
+  height: 100vh;
+  background: #22282d;
+`;
+
+const LogoContainer = styled.div`
+  display: flex;
+  width: 100%;
+  height: 20%;
+  flex-direction: row;
+  align-items: center;
+  margin-left: 22%;
+`;
+
+const LeftSection: React.FC = () => {
+  return (
+    <Wrapper>
+      <LogoContainer>
+        <img src={logo} alt="logo" />
+      </LogoContainer>
+    </Wrapper>
+  );
+};
+
+export default LeftSection;
